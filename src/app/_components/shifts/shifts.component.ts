d4f525d08ecf0d140b93dc47a8928577852c09c8
Nishil <nishilfoo@gmail.com>
2018-02-02 12:17:35 -0800

    dynamic modal

--- src/app/_components/shifts/shifts.component.ts
+++ src/app/_components/shifts/shifts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-shifts',
@@ -6,10 +7,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shifts.component.css']
 })
 export class ShiftsComponent implements OnInit {
-
-  constructor() { }
+  modalData:any;
+  showModal:boolean;
+  dates:string[];
+  shiftType:string;
+  subTypes:any;
+  constructor(private router:Router) { }
 
   ngOnInit() {
+    this.showModal = false;
+    this.modalData = {};
+    this.dates = ["2018-02-03","2018-02-05","2018-02-06","2018-02-07","2018-02-08"];
+    this.shiftType = this.getShift(this.router.url);
+    this.subTypes = {
+      dismantle:["small","large"],
+      test:["HDD","RAM","Misc"],
+      build:["Novice","Advanced"],
+      clean:["cleaning"]
+    }
   }
 
+  getShift(url:string):string{
+    return url.split('/')[2];
+  }
+
+  updateModalData(eventObject):void{
+    var shifts = [];
+    var subTypeArray = this.subTypes[this.shiftType];
+    for (var subType in subTypeArray){
+      shifts.push({subType:subTypeArray[subType], availableSlots:"2"})
+    }
+    this.modalData = {
+      date:eventObject.target.attributes['data-date'].value,
+      shiftType:this.shiftType,
+      shifts:shifts,
+      startTime:eventObject.target.attributes['data-start-time'].value,
+      endTime:eventObject.target.attributes['data-end-time'].value,
+      showModal:!this.showModal
+    }
+    //console.log(this.modalData);
+  }
+
+  returnModaldata(){
+    return this.modalData;
+  }
 }
--- src/app/_components/signup-modal/signup-modal.component.ts
+++ src/app/_components/signup-modal/signup-modal.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-signup-modal',
   templateUrl: './signup-modal.component.html',
   styleUrls: ['./signup-modal.component.css']
 })
-export class SignupModalComponent implements OnInit {
 
-  constructor() { }
+export class SignupModalComponent implements OnInit {
+  modalData:any;
+  showModal:boolean;
+
+  @Input()
+  set setModal(modalData: any) {
+    this.modalData = modalData;
+    this.showModal = modalData.showModal;
+}
 
   ngOnInit() {
+    this.showModal = false;
   }
 
 }
