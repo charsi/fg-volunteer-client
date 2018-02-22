f2ad8e2709df6b0225b72dbf960b97c60ec3e727
Nishil <nishilfoo@gmail.com>
2018-02-22 11:35:08 -0800

    user add form: add validation

--- src/app/_components/admin-users-add/admin-users-add.component.ts
+++ src/app/_components/admin-users-add/admin-users-add.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+import { User } from '../../_models/user';
+import { UserService } from '../../_services/user.service';
+
 @Component({
   selector: 'app-admin-users-add',
   templateUrl: './admin-users-add.component.html',
   styleUrls: ['./admin-users-add.component.css']
 })
 export class AdminUsersAddComponent implements OnInit {
-
+  model = new User();
+  submitted = false;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSubmit(){
+    console.log(this.model);
   }
 
 }
